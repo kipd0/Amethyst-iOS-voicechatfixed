2901e47ccf20f1f2f6055fed8c6f792ccc8f42c1
kipd0 <laith28dev@gmail.com>
2026-09-03 10:50:27 +0300

    Refactor PLPreferences.m for improved readability
    Signed-off-by: kipd0 <laith28dev@gmail.com>

--- Natives/PLPreferences.m
+++ Natives/PLPreferences.m
@@ -17,6 +17,7 @@
             @"cosmetica": @YES,
             @"debug_logging": @(!CONFIG_RELEASE),
         }.mutableCopy,
+
         @"video": @{ // Video & Audio
             @"renderer": @"auto",
             @"resolution": @(100),
@@ -25,11 +26,16 @@
             @"fullscreen_airplay": @YES,
             @"silence_other_audio": @NO,
             @"silence_with_switch": @NO,
+            @"fix_simple_voice_chat_mod": @NO,
             @"allow_microphone": @NO
         }.mutableCopy,
+
         @"control": @{
             @"default_ctrl": @"default.json",
-            @"control_safe_area": UIApplication.sharedApplication ? NSStringFromUIEdgeInsets(getDefaultSafeArea()) : @"",
+            @"control_safe_area":
+                UIApplication.sharedApplication
+                    ? NSStringFromUIEdgeInsets(getDefaultSafeArea())
+                    : @"",
             @"default_gamepad_ctrl": @"default.json",
             @"controller_type": @"xbox",
             @"hardware_hide": @YES,
@@ -47,6 +53,7 @@
             @"gyroscope_invert_x_axis": @NO,
             @"gyroscope_sensitivity": @(100)
         }.mutableCopy,
+
         @"java": @{
             @"java_homes": @{
                 @"0": @{
@@ -54,15 +61,29 @@
                     @"1_17_newer": @"17",
                     @"execute_jar": @"8"
                 }.mutableCopy,
+
                 @"8": @"internal",
                 @"17": @"internal",
                 @"21": @"internal"
             }.mutableCopy,
+
             @"java_args": @"",
             @"env_variables": @"",
-            @"auto_ram": @(!getEntitlementValue(@"com.apple.private.memorystatus")),
-            @"allocated_memory": [NSNumber numberWithFloat:roundf((NSProcessInfo.processInfo.physicalMemory / 1048576) * 0.25)]
+
+            @"auto_ram":
+                @(!getEntitlementValue(
+                    @"com.apple.private.memorystatus"
+                )),
+
+            @"allocated_memory":
+                [NSNumber numberWithFloat:
+                    roundf(
+                        (NSProcessInfo.processInfo.physicalMemory / 1048576)
+                        * 0.25
+                    )
+                ]
         }.mutableCopy,
+
         @"internal": @{
             @"isolated": @NO,
             @"latest_version": [NSDictionary new]
@@ -73,27 +94,38 @@
         // Preferences that cannot be isolated
         NSDictionary *general = @{
             @"game_directory": @"default",
-            @"hidden_sidebar": @(realUIIdiom == UIUserInterfaceIdiomPhone),
+
+            @"hidden_sidebar":
+                @(realUIIdiom == UIUserInterfaceIdiomPhone),
+
             @"appicon": @"AppIcon-Light"
         };
-        [defaults[@"general"] addEntriesFromDictionary:general];
+
+        [defaults[@"general"]
+            addEntriesFromDictionary:general];
 
         defaults[@"java"][@"manage_runtime"] = @""; // stub
+
         defaults[@"debug"] = @{
             @"debug_always_attached_jit": @NO,
             @"debug_skip_wait_jit": @NO,
             @"debug_hide_home_indicator": @NO,
-            @"debug_ipad_ui": @(realUIIdiom == UIUserInterfaceIdiomPad),
+
+            @"debug_ipad_ui":
+                @(realUIIdiom == UIUserInterfaceIdiomPad),
+
             @"debug_auto_correction": @YES,
             @"debug_show_layout_bounds": @NO,
             @"debug_show_layout_overlap": @NO
         }.mutableCopy;
+
         defaults[@"warnings"] = @{
             @"local_warn": @YES,
             @"mem_warn": @YES,
             @"auto_ram_warn": @YES,
             @"limited_ram_warn": @YES
         }.mutableCopy;
+
         // TODO: isolate this or add account picker into profile editor(?)
         defaults[@"internal"][@"selected_account"] = @"";
     }
@@ -101,163 +133,323 @@
     return defaults;
 }
 
-+ (id)getPreference:(NSString *)key from:(NSDictionary *)pref {
++ (id)getPreference:(NSString *)key
+               from:(NSDictionary *)pref {
+
     for (NSDictionary *section in pref.allValues) {
-        if ([section isKindOfClass:NSDictionary.class] && section[key]) {
+        if (
+            [section isKindOfClass:NSDictionary.class]
+            && section[key]
+        ) {
             return section[key];
         }
     }
+
     return nil;
 }
 
-+ (id)getOldLayoutPreference:(NSString *)key from:(NSDictionary *)pref {
++ (id)getOldLayoutPreference:(NSString *)key
+                        from:(NSDictionary *)pref {
+
     // Find preference in the root dictionary first
     if (pref[key]) {
         return pref[key];
     }
+
     // Find preference in subdictionaries
-    id value = [self getPreference:key from:pref];
+    id value =
+        [self getPreference:key from:pref];
+
     if (!value) {
-        NSLog(@"[PLPreferences] Migrator could not find preference %@", key);
+        NSLog(
+            @"[PLPreferences] Migrator could not find preference %@",
+            key
+        );
     }
+
     return value;
 }
 
 - (id)initWithGlobalPath:(NSString *)path {
     self = [super init];
+
     self.globalPath = path;
-    self.globalPref = [NSMutableDictionary dictionaryWithContentsOfFile:path];
+
+    self.globalPref =
+        [NSMutableDictionary
+            dictionaryWithContentsOfFile:path];
+
     [self saveGlobalPref];
+
     return self;
 }
 
 - (id)initWithAutomaticMigrator {
     self = [super init];
-    self.globalPath = [@(getenv("POJAV_HOME")) stringByAppendingPathComponent:@"launcher_preferences_v2.plist"];
-    NSMutableDictionary *pref = [NSMutableDictionary dictionaryWithContentsOfFile:self.globalPath];
 
-    NSString *oldPath = [@(getenv("POJAV_HOME")) stringByAppendingPathComponent:@"launcher_preferences.plist"];
-    NSMutableDictionary *oldPref = [NSMutableDictionary dictionaryWithContentsOfFile:oldPath];
+    self.globalPath =
+        [@(getenv("POJAV_HOME"))
+            stringByAppendingPathComponent:
+                @"launcher_preferences_v2.plist"];
+
+    NSMutableDictionary *pref =
+        [NSMutableDictionary
+            dictionaryWithContentsOfFile:self.globalPath];
+
+    NSString *oldPath =
+        [@(getenv("POJAV_HOME"))
+            stringByAppendingPathComponent:
+                @"launcher_preferences.plist"];
+
+    NSMutableDictionary *oldPref =
+        [NSMutableDictionary
+            dictionaryWithContentsOfFile:oldPath];
 
     if (pref || !oldPref[@"env_vars"]) {
         // Initialize or load existing v2 layout
         self.globalPref = pref;
+
     } else {
-        NSDebugLog(@"[PLPreferences] Migrating to %@", self.globalPath.lastPathComponent);
+        NSDebugLog(
+            @"[PLPreferences] Migrating to %@",
+            self.globalPath.lastPathComponent
+        );
+
         // Perform migration from v1 layout
-        self.globalPref = [NSMutableDictionary new];
+        self.globalPref =
+            [NSMutableDictionary new];
+
         for (NSString *section in self.globalPref.allKeys) {
-            for (NSString *key in self.globalPref[section].allKeys) {
-                id value = [PLPreferences getOldLayoutPreference:key from:oldPref];
+            for (
+                NSString *key
+                in self.globalPref[section].allKeys
+            ) {
+                id value =
+                    [PLPreferences
+                        getOldLayoutPreference:key
+                        from:oldPref];
+
                 if (value) {
-                    self.globalPref[section][key] = value;
+                    self.globalPref[section][key] =
+                        value;
                 }
             }
         }
     }
 
     [self saveGlobalPref];
+
     return self;
 }
 
-- (id)setDefaultsForPref:(NSMutableDictionary *)pref global:(BOOL)global {
-    NSMutableDictionary<NSString *, NSMutableDictionary *> *defaults = [PLPreferences defaultPrefForGlobal:global];
+- (id)setDefaultsForPref:(NSMutableDictionary *)pref
+                  global:(BOOL)global {
+
+    NSMutableDictionary<
+        NSString *,
+        NSMutableDictionary *
+    > *defaults =
+        [PLPreferences
+            defaultPrefForGlobal:global];
+
     if (!pref) {
-        NSLog(@"[PLPreferences] Initializing default values for %@ preferences", global ? @"global" : @"isolated");
+        NSLog(
+            @"[PLPreferences] Initializing default values "
+             "for %@ preferences",
+            global ? @"global" : @"isolated"
+        );
+
         return defaults;
     }
 
     for (NSString *section in defaults.allKeys) {
         if (!pref[section]) {
-            NSDebugLog(@"[PLPreferences] Set default values for section %@", section);
-            pref[section] = defaults[section];
+            NSDebugLog(
+                @"[PLPreferences] Set default values "
+                 "for section %@",
+                section
+            );
+
+            pref[section] =
+                defaults[section];
+
             continue;
         }
-        for (NSString *key in defaults[section].allKeys) {
-            if (pref[section][key]) continue;
-            id value = defaults[section][key];
-            NSDebugLog(@"[PLPreferences] Set default vaule: %@: %@", key, value);
-            pref[section][key] = value;
+
+        for (
+            NSString *key
+            in defaults[section].allKeys
+        ) {
+            if (pref[section][key])
+                continue;
+
+            id value =
+                defaults[section][key];
+
+            NSDebugLog(
+                @"[PLPreferences] Set default vaule: %@: %@",
+                key,
+                value
+            );
+
+            pref[section][key] =
+                value;
         }
     }
+
     return pref;
 }
 
 - (void)setGlobalPref:(NSMutableDictionary *)pref {
-    _globalPref = [self setDefaultsForPref:pref global:YES];
+    _globalPref =
+        [self
+            setDefaultsForPref:pref
+            global:YES];
 }
 
 - (void)setInstancePref:(NSMutableDictionary *)pref {
-    _instancePref = [self setDefaultsForPref:pref global:NO];
+    _instancePref =
+        [self
+            setDefaultsForPref:pref
+            global:NO];
 }
 
 - (void)toggleIsolationForced:(BOOL)force {
-    NSMutableDictionary *instancePref = [NSMutableDictionary dictionaryWithContentsOfFile:self.instancePath];
-    if (force || [instancePref[@"internal"][@"isolated"] boolValue]) {
-        NSLog(@"[PLPreferences] Using isolated preferences from %@", self.instancePath.stringByResolvingSymlinksInPath);
-        self.instancePref = instancePref;
+    NSMutableDictionary *instancePref =
+        [NSMutableDictionary
+            dictionaryWithContentsOfFile:self.instancePath];
+
+    if (
+        force
+        ||
+        [instancePref[@"internal"][@"isolated"]
+            boolValue]
+    ) {
+        NSLog(
+            @"[PLPreferences] Using isolated preferences from %@",
+            self.instancePath.stringByResolvingSymlinksInPath
+        );
+
+        self.instancePref =
+            instancePref;
+
         if (!instancePref) {
             // Copy preferences from the global one
-            for (NSString *section in self.instancePref) {
-                for (NSString *key in self.instancePref[section].allKeys) {
-                    self.instancePref[section][key] = self.globalPref[section][key];
+            for (
+                NSString *section
+                in self.instancePref
+            ) {
+                for (
+                    NSString *key
+                    in self.instancePref[section].allKeys
+                ) {
+                    self.instancePref[section][key] =
+                        self.globalPref[section][key];
                 }
             }
         }
 
         // Declare that itself is isolated
-        self.instancePref[@"internal"][@"isolated"] = @YES;
+        self.instancePref[@"internal"][@"isolated"] =
+            @YES;
 
         [self saveInstancePref];
+
     } else if (self.instancePref) {
-        NSLog(@"[PLPreferences] Using global preferences");
-        _instancePref = nil;
+        NSLog(
+            @"[PLPreferences] Using global preferences"
+        );
+
+        _instancePref =
+            nil;
     }
 }
 
 - (id)getObject:(NSString *)key {
-    id value = [self.instancePref valueForKeyPath:key];
+    id value =
+        [self.instancePref
+            valueForKeyPath:key];
+
     if (!value) {
-        value = [self.globalPref valueForKeyPath:key];
+        value =
+            [self.globalPref
+                valueForKeyPath:key];
     }
+
     if (!value) {
-        NSLog(@"[PLPreferences] Getter could not find preference %@", key);
+        NSLog(
+            @"[PLPreferences] Getter could not find preference %@",
+            key
+        );
     }
+
     return value;
 }
 
-- (BOOL)setObject:(NSString *)key value:(id)value {
-    if ([self.instancePref valueForKeyPath:key]) {
-        [self.instancePref setValue:value forKeyPath:key];
+- (BOOL)setObject:(NSString *)key
+            value:(id)value {
+
+    if (
+        [self.instancePref
+            valueForKeyPath:key]
+    ) {
+        [self.instancePref
+            setValue:value
+            forKeyPath:key];
+
         [self saveInstancePref];
+
         return YES;
-    } else if ([self.globalPref valueForKeyPath:key]) {
-        [self.globalPref setValue:value forKeyPath:key];
+
+    } else if (
+        [self.globalPref
+            valueForKeyPath:key]
+    ) {
+        [self.globalPref
+            setValue:value
+            forKeyPath:key];
+
         [self saveGlobalPref];
+
         return YES;
     }
-    NSLog(@"[PLPreferences] Setter could not find preference %@", key);
+
+    NSLog(
+        @"[PLPreferences] Setter could not find preference %@",
+        key
+    );
+
     return NO;
 }
 
 - (void)reset {
     if (self.instancePref) {
-        [NSFileManager.defaultManager removeItemAtPath:self.instancePath error:nil];
+        [NSFileManager.defaultManager
+            removeItemAtPath:self.instancePath
+            error:nil];
+
         [self toggleIsolationForced:YES];
+
         // Only reset isolated values
         return;
     }
 
-    self.globalPref = nil;
+    self.globalPref =
+        nil;
+
     [self saveGlobalPref];
 }
 
 - (void)saveGlobalPref {
-    [self.globalPref writeToFile:self.globalPath atomically:YES];
+    [self.globalPref
+        writeToFile:self.globalPath
+        atomically:YES];
 }
 
 - (void)saveInstancePref {
-    [self.instancePref writeToFile:self.instancePath atomically:YES];
+    [self.instancePref
+        writeToFile:self.instancePath
+        atomically:YES];
 }
 
 @end
