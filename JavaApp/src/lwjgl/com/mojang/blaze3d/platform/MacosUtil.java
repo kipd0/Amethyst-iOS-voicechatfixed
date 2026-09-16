dfe32d1c8844d9e669ffd32995f771e148508dfb
kipd0 <192927056+kipd0@users.noreply.github.com>
2026-09-16 20:28:40 +0300

    Add files via upload
    Signed-off-by: kipd0 <192927056+kipd0@users.noreply.github.com>

---
+++ JavaApp/src/lwjgl/com/mojang/blaze3d/platform/MacosUtil.java
@@ -0,0 +1,14 @@
+package com.mojang.blaze3d.platform;
+
+public final class MacosUtil {
+    public static final boolean IS_MACOS = false;
+
+    private MacosUtil() {
+    }
+
+    public static void disableCloseWindowMenuItem() {
+    }
+
+    public static void setCtrlClickEmulatesRightClick(boolean enabled) {
+    }
+}
