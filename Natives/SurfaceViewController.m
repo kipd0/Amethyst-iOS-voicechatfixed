1ae2b3ab794a48e1af72c54b8f6628db36a5d24e
kipd0 <192927056+kipd0@users.noreply.github.com>
2026-09-17 07:43:13 +0300

    Update SurfaceViewController.m
    Signed-off-by: kipd0 <192927056+kipd0@users.noreply.github.com>

--- Natives/SurfaceViewController.m
+++ Natives/SurfaceViewController.m
@@ -185,7 +185,8 @@ static GameSurfaceView* pojavWindow;
     self.mousePointerView.userInteractionEnabled = NO;
     [self.touchView addSubview:self.mousePointerView];
 
-    self.inputTextField = [[TrackedTextField alloc] initWithFrame:CGRectMake(0, -32.0, self.view.frame.size.width, 30.0)];
+    sself.inputTextField = [[TrackedTextField alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 30.0)];
+    self.inputTextField.alpha = 0.02f;
     self.inputTextField.backgroundColor = UIColor.secondarySystemBackgroundColor;
     self.inputTextField.delegate = self;
     self.inputTextField.font = [UIFont fontWithName:@"Menlo-Regular" size:20];
